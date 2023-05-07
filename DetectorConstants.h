4d3f40079c68aa1da772388b410aefd28af95e1c
Stuart Fegan <41978718+sfegan84@users.noreply.github.com>
2023-05-07 22:14:22 +0100

    Adding Kaon minimum momentum cut

--- DetectorConstants.h
+++ DetectorConstants.h
@@ -6,6 +6,7 @@ const double EC_sampling_frac = 0.31; //for photons
 
 const double prot_accept_mom_lim = 0.3;  //proton momentum threshold
 const double pion_accept_mom_lim = 0.15; //pion momentum threshold
+const double kaon_accept_mom_lim = 0.15; //kaon momentum threshold
 
 
 #endif
